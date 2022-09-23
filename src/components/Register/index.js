383b18ea1b26a871e507af2024f63a26a4e1de51
faiza-zakir <faiza.prism@gmail.com>
2022-09-23 19:15:39 +0500

    add zoho form

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,22 +1,8 @@
 import React, { Component } from "react";
 import _data from "../../data";
-import { CountryCode } from "../../data/countries";
 import ZohoBannerForm from "../ZohoBannerForm/ZohoBannerForm";
 
 class Register extends Component {
-  componentDidMount() {
-    /**
-     * Your ajax will goes here to get data then call setState
-     */
-    this.setState({
-      contact: _data.contact,
-    });
-
-    this.setState({
-      countrycodeData: CountryCode,
-    });
-  }
-
   render() {
     return (
       <React.Fragment>
@@ -52,86 +38,7 @@ class Register extends Component {
                 </div>
               </div>
               <div className="col-md-6">
-                {/* <form id="contactForm1" className="contact-us-form">
-                  <div className="row">
-                    <div className="col-12">
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          className="form-control"
-                          name="name"
-                          placeholder="Name"
-                          required="required"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-group">
-                        <input
-                          type="email"
-                          className="form-control"
-                          name="email"
-                          placeholder="Your Email Address"
-                          required="required"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-2 pr-0">
-                      <div className="form-group">
-                        <select
-                          name="country_code"
-                          className="form-control"
-                          required="required"
-                        >
-                          {CountryCode.map((x, i) => (
-                            <option value={x.dial_code} key={i}>
-                              {x.dial_code} - {x.name}
-                            </option>
-                          ))}
-                        </select>
-                      </div>
-                    </div>
-                    <div className="col-10 pl-0">
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          name="phone"
-                          className="form-control"
-                          id="phone"
-                          placeholder="Your Phone Number"
-                        />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="form-group">
-                        <input
-                          type="radio"
-                          id="agreement"
-                          name="agreement"
-                          className="mr-2"
-                          value="agree"
-                        />
-                        <label for="agreement">
-                          I accept the terms of the User Agreement and Privacy
-                          Policy
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-12 mt-3">
-                      <button
-                        type="submit"
-                        className="register-btn"
-                        id="btnContactUs"
-                      >
-                        Register
-                      </button>
-                    </div>
-                  </div>
-                </form> */}
                 <ZohoBannerForm />
-                <p className="form-message"></p>
               </div>
             </div>
           </div>
