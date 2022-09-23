64a3ff4a1a4da01dd4494a697711980d075bd3bd
faiza-zakir <faiza.prism@gmail.com>
2022-09-23 13:10:57 +0500

    add loader

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./routers";
+import BounceLoader from "react-spinners/BounceLoader";
 
 function App() {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    setLoader(false);
+  });
   return (
     <React.Fragment>
-      <Routes />
+      {loader ? (
+        <div
+          className={`d-flex flex-column text-center align-items-center justify-content-center`}
+          style={{
+            position: "absolute",
+            zIndex: 99999,
+            height: "100%",
+            width: "100%",
+            background: "rgba(49, 135, 196, 0.3)",
+          }}
+        >
+          <BounceLoader color={"#071c35"} size={100} />
+        </div>
+      ) : (
+        <Routes />
+      )}
     </React.Fragment>
   );
 }
 
 export default App;
-
-// import React, { Suspense } from "react";
-// import { BrowserRouter } from "react-router-dom";
-// import Routes from "./routers";
-// import ScrollToTop from "./data/ScrollToTop";
-// import BounceLoader from "react-spinners/BounceLoader";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Suspense
-//         fallback={
-//           <div
-//             className={`d-flex flex-column text-center align-items-center justify-content-center`}
-//             style={{
-//               position: "absolute",
-//               zIndex: 99999,
-//               height: "100%",
-//               width: "100%",
-//               background: "rgba(49, 135, 196, 0.3)",
-//             }}
-//           >
-//             <BounceLoader color={"#071c35"} size={100} />
-//           </div>
-//         }
-//       >
-//         <ScrollToTop />
-//         <Routes />
-//       </Suspense>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
